ee2ccf6aaa517ce6cb5f3f5ff63bcd8defa675e5
Abhishek Nishad <abhishekk77625@gmail.com>
2026-03-03 18:37:05 +0530

    pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const express = require("express");
 const { userAuth } = require("../middleWares/auth");
 const ConnectionRequestModel = require("../model/connectionRequest");
 const userRouter = express.Router()
+const User = require("../model/user")
 
 const USER_PUBLIC_DATA = "Fname Lname age gender"
 
@@ -62,5 +63,66 @@ userRouter.get("/user/connections", userAuth, async(req, res)=>{
     }
 
 } )
+
+// Feed api - Pagination 
+
+userRouter.get( "/feed", userAuth, async(req, res) =>{
+
+    try {
+
+        const loggedInUser = req.user;
+        // Pagination
+        const page = parseInt(req.query.page) || 1;
+        const limit = 3;
+
+        const skip = (page - 1) * limit;
+        
+        const connectionRequest = await ConnectionRequestModel.find({
+            
+            $or:[ {fromUserId: loggedInUser._id},
+                {toUserId: loggedInUser._id}
+            ]
+            
+        }).select("fromUserId toUserId")
+
+        
+        const hideUserfromFeed = new Set();
+        connectionRequest.forEach( (req) =>{
+
+            hideUserfromFeed.add(req.fromUserId.toString());
+            hideUserfromFeed.add(req.toUserId.toString());
+
+        })
+
+        // console.log(hideUserfromFeed);
+        
+        const users = await User.find({
+            $and:[
+                { _id: { $nin: Array.from(hideUserfromFeed)}},
+                {_id: {$ne: loggedInUser._id}}
+            ]
+        }).select(USER_PUBLIC_DATA)
+        .limit(limit)
+        .skip(skip)
+        // .sort({ createdAt: -1 })   // latest first
+        
+        res.json({
+            message:"Total people in feed",
+            data:users
+        });
+
+
+    } catch (err) {
+
+        res.status(404).send(err)
+        
+    }
+
+})
+
+// now Pagination - help to divide the large dataset into smaller chunk and managable dataset
+
+
+
 
 module.exports  = userRouter;
