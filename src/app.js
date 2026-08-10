6b77f51149a51e41ee0f2e11731c3a657e5868bb
Abhishek Nishad <abhishekk77625@gmail.com>
2026-08-10 16:02:56 +0530

    update origin

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ require('dotenv').config()
 const port = process.env.PORT || 5500;
 
 const corsOptions = {
-  origin: ['http://localhost:5173', "https://devloper-tinder.vercel.app"], // Allow request from frontend
+  origin: ["https://devloper-tinder.vercel.app"], // Allow request from frontend
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], // Explicitly allow PATCH
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true // Enable 
@@ -57,7 +57,7 @@ app.get("/user", async (req, res) => {
 
     try {
         // console.log(userEmail);
-        const user = await User.findOne({ emailId: userEmail }); // find One user
+        const user = await User.findOne({ userEmail }); // find One user
         if (!user) {
             res.status(404).send("User not Found")
 
