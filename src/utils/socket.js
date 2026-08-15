e6fe7d639d918988ab026769a7d7627cf0e4b231
Abhishek Nishad <abhishekk77625@gmail.com>
2026-08-15 10:56:27 +0530

    fix origin urL

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -18,7 +18,7 @@ const setupWebSocket = (server) => {
         
     const io = socket(server, {
         cors: {
-            origin: "http://localhost:5173"
+            origin: "https://devloper-tinder.vercel.app"
 
         },
     });
